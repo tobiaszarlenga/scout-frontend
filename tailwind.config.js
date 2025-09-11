3a5dc69b072ea105d724c9d5aa32e5b51bffa0f4
Tobias Zarlenga <tobizarlenga@gmail.com>
2025-09-11 17:40:43 -0300

    chore(init): create next app

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: "class",
+  content: [
+    "./app/**/*.{ts,tsx,js,jsx,mdx}",
+    "./components/**/*.{ts,tsx,js,jsx,mdx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
